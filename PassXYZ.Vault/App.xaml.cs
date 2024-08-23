6b6e09f9ef0812621e0ca20b7e9aa62034fefaa7
David Bendixen <dwbgithub@gmail.com>
2024-08-23 05:44:19 -0400

    Add project files.

---
+++ PassXYZ.Vault/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+namespace PassXYZ.Vault;
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+        MainPage = new MainPage();
+
+
+
+    }
+    //protected override Window CreateWindow(IActivationState?
+    //  activationState)                                       //(1)
+    //{
+    //    return new PxWindow(new MainPage());
+    //}
+
+
+    protected override void OnStart()
+    {                    //(1)
+        Debug.WriteLine("PassXYZ.Vault.App: OnStart");
+    }
+    protected override void OnSleep()
+    {                    //(2)
+        Debug.WriteLine("PassXYZ.Vault.App: OnSleep");
+    }
+    protected override void OnResume()
+    {                   //(3)
+        Debug.WriteLine("PassXYZ.Vault.App: OnResume");
+    }
+}
